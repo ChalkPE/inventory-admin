217ffe075ce7f08a285667653961bdaf53123064
ChalkPE <chalk@chalk.pe>
2018-01-11 02:54:49 +0900

    add store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import store from './store'
 import router from './router'
 
 export default new Vue({
+  store,
   router,
   el: '#root',
   render: (h) => h(App)
