f80601ed101cceb2d383896a6e0b9eac9b2eecdc
ChalkPE <chalk@chalk.pe>
2018-01-16 22:21:08 +0900

    move api.js

--- src/menus/Goods.vue
+++ src/menus/Goods.vue
@@ -2,6 +2,7 @@
 </template>
 
 <script>
+// 상품명	판매가	판매자	등록일 / 수정일	삭제
 export default {
 
 }
--- src/menus/Member.vue
+++ src/menus/Member.vue
@@ -2,6 +2,8 @@
 </template>
 
 <script>
+// 아이디/닉네임	이름	회원가입일	최종로그인	이메일	뉴스레터 동의	성별	국가	제재
+
 export default {
 
 }
